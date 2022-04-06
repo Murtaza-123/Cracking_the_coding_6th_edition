6dce76b676e1d187ab5069633138d414d83b915d
Murtaza-123 <murtaza-123@github.com>
2022-04-07 02:23:32 +0500

    change count_duplicate method to remove duplicate

---
+++ Runner.java
@@ -0,0 +1,31 @@
+public class Runner{
+    public static void main(String[] args){
+        LinkedList list = new LinkedList();
+        list.insert(1);
+        list.insert(2);
+        list.insert(3);
+        list.insert(2);
+       // list.prepend(20);
+        //list.append(30);
+        //list.addAfter(10, 15);
+        //list.addBefore(17,15);
+        //list.Pop();
+        //list.remove_end();
+        //list.pop_by_value(30);
+        list.insert(2);
+        //list.append(20);
+        //list.lenght();
+        //list.get(3);
+        //list.get_kth_to_end_elemnet(4);
+       // list.show();
+        //list.delete();
+        //list.show();
+        list.insert(4);
+        list.insert(1);
+        list.show();
+        list.count_duplicate();
+        list.show();
+
+        
+    }
+}
